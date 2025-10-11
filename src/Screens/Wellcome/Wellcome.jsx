2214819167a5911cafc220ce3d64d02895216dbc
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-10-11 14:30:36 -0300

    importando o icone

--- src/Screens/Wellcome/Wellcome.jsx
+++ src/Screens/Wellcome/Wellcome.jsx
@@ -6,6 +6,7 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import { useFonts } from '@expo-google-fonts/montserrat/useFonts';
 import { Montserrat_400Regular } from '@expo-google-fonts/montserrat/400Regular';
 import { Montserrat_700Bold } from '@expo-google-fonts/montserrat/700Bold';
+import AntDesign from '@expo/vector-icons/AntDesign';
 export const Wellcome = () => {
     let [fontsLoaded] = useFonts({
         Montserrat_400Regular,
