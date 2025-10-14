b0e34e71e50430591cde03a0a0c6f2695033b9fe
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-14 01:37:37 -0300

    criando um arquivo de configuração

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      'babel-preset-expo', // 👈 necessário para projetos Expo
+    ],
+    plugins: [
+      // Permite usar importações automáticas e reexportações
+      'react-native-reanimated/plugin', // 👈 obrigatório se usar Reanimated
+      [
+        'module-resolver',
+        {
+          root: ['./src'], // 👈 permite usar imports absolutos, ex: import X from "components/X"
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/Screens',
+            '@assets': './src/assets',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  };
+};
