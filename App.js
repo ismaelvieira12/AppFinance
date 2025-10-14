8aa84fdc775e4bc1a64a6366aa775279b524bd44
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-13 23:36:05 -0300

    update

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { Menu} from './src/components/menu/Menu.jsx';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Wellcome } from './src/Screens/Wellcome/Wellcome.jsx';
+const Drawer = createDrawerNavigator();
 import * as React from 'react';
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Menu />
-      {/*
-        <Wellcome />
-      */}
+      <Drawer.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+          <Drawer.Screen name="Home" component={Wellcome} />
+      </Drawer.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
