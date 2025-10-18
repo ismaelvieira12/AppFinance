3def4a05b19c39d6fc001fe74850b53333fe2b54
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-10-18 14:01:30 -0300

    Criando um arquivo de cor para o acessar globalmente.

---
+++ src/paletaColor/ColorGlobal.js
@@ -0,0 +1,8 @@
+
+export const ColorGlobal = [
+    AzulEscuro = "#2364AA",
+    AzulMaisClaro = "#3DA5D9",
+    AzulCianeto = "#73BFB8",
+    AmareloNormal = "#FEC601",
+    LaranjaEscuro = "#EA7317"
+]
