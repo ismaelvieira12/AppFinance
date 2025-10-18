93024d4061cc64de1792e657c96f4261e205abeb
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-10-18 12:42:14 -0300

    Colocando a pagina inicial

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { MetasScreen } from './src/Screens/MetasScreen/MetasScreen.jsx';
+import { InitialPaination } from './src/Screens/InitialPagination/InitialPaination.jsx';
 const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+        <Drawer.Screen name='InicialPagination' component={InitialPaination} />
         <Drawer.Screen name="Home" component={Wellcome} />
         <Drawer.Screen name='Metas' component={MetasScreen} />
       </Drawer.Navigator>
