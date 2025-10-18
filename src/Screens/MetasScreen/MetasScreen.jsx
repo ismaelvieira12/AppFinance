e1c4393c172fda14123cffbe990c043366960bd3
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-10-18 14:53:05 -0300

    Criando o layout da pagina inical

--- src/Screens/MetasScreen/MetasScreen.jsx
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -1,13 +1,29 @@
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { styles } from './MetasScreenStyle'
-import { StatusBar } from 'react-native'
+import { StatusBar, Image } from 'react-native'
 import React from 'react'
 
 export const MetasScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.boxImage}></View>
-      <StatusBar barStyle={'dark-content'}/>
+      <View style={styles.boxImage}>
+        <Image
+          source={require('../../../assets/imgs/imagem-inicialPNG03.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.boxInfor}>
+        <View>
+          <Text style={styles.title}>Um futuro Mais seguro para você</Text>
+        </View>
+        <Text style={styles.subTitle}>Tendo previsibilidade financeira você garante uma vida muito mais tranquila e segura.</Text>
+
+        <View style={styles.boxButtom}>
+
+        </View>
+      </View>
+      <StatusBar barStyle="dark-content" />
     </View>
   )
 }
--- src/Screens/MetasScreen/MetasScreenStyle.js
+++ src/Screens/MetasScreen/MetasScreenStyle.js
@@ -4,12 +4,18 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: ColorGlobal.FundoBody,
+        backgroundColor: ColorGlobal.FundoCards,
     },
     boxImage: {
-        backgroundColor: ColorGlobal.FundoCards,
-        height: "40%",
+        backgroundColor: ColorGlobal.FundoBody,
+        height: "60%",
         borderBottomEndRadius: 70,
-        borderBottomLeftRadius: 70
+        borderBottomLeftRadius: 70,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        
+        height: '100%'
     }
 })
