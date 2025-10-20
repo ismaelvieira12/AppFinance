4532e4c4db91cfe440b06d71a24ecbf64038d105
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-19 23:46:20 -0300

    Criando um layout

--- src/Screens/login/ScreenLogin.jsx
+++ src/Screens/login/ScreenLogin.jsx
@@ -1,11 +1,15 @@
 import { Text, View } from 'react-native'
 import { styles } from './ScreenLoginStyle'
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 
 export const ScreenLogin = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.textLogin}>ScreenLogin</Text>
+      <View>
+        <TouchableOpacity></TouchableOpacity>
+      </View>
     </View>
   )
 }
