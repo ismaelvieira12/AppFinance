26207671ac20b5440e77d05da4f87f484949363c
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-20 22:29:03 -0300

    update

--- src/Screens/login/ScreenLoginStyle.js
+++ src/Screens/login/ScreenLoginStyle.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import {ColorGlobal} from '../../Styles/ColorsGlobal'
+import {ColorGlobal} from '../../paletaColor/ColorGlobal'
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
