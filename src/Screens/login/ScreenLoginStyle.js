3351c489a5132249af48906613a0750c2abcf9fd
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-22 22:23:02 -0300

    update

--- src/Screens/login/ScreenLoginStyle.js
+++ src/Screens/login/ScreenLoginStyle.js
@@ -19,7 +19,7 @@ export const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         resizeMode: 'contain',
-        marginTop: 20,
+        marginTop: 0,
     },
     nputLogin: {
         width: 350,
