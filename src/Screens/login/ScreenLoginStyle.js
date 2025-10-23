833203be9898e14943ca1f2b20d9121e4a937b64
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-22 22:27:02 -0300

    Alinhado

--- src/Screens/login/ScreenLoginStyle.js
+++ src/Screens/login/ScreenLoginStyle.js
@@ -2,8 +2,8 @@ import { StyleSheet } from "react-native";
 import {ColorGlobal} from '../../paletaColor/ColorGlobal'
 export const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        // flex: 1,
+        // justifyContent: 'center',
         alignItems: "center",
         backgroundColor: ColorGlobal.TesteColor
     },
