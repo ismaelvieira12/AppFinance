b1b5039d1d154b54c6bcb3536267bb376c207788
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-24 23:52:28 -0300

    Update

--- src/Screens/register/ScreenRegister.jsx
+++ src/Screens/register/ScreenRegister.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { ColorGlobal } from '../../paletaColor/ColorGlobal'
+import { styles } from '../login/ScreenLoginStyle'
+
 
 export const ScreenRegister = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={styles.container}>
       <Text style={{fontSize: 56, color: ColorGlobal.AmareloNormal}}>ScreenRegister</Text>
     </View>
   )
--- src/Screens/register/ScreenRegisterStyle.js
+++ src/Screens/register/ScreenRegisterStyle.js
@@ -1,4 +1,11 @@
 import { StyleSheet} from 'react-native'
+import { ColorGlobal } from '../../paletaColor/ColorGlobal';
 
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: ColorGlobal.TesteColor,
+    },
+})
