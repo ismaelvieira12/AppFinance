ba273b2d2cd20ad95bfe775484de07d07e7756d6
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-25 21:44:04 -0300

    Update

--- src/Screens/login/ScreenLogin.jsx
+++ src/Screens/login/ScreenLogin.jsx
@@ -29,7 +29,7 @@ export const ScreenLogin = () => {
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={{flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center', padding: 40}}>
-        <Entypo name="chevron-left" size={40} color={ColorGlobal.ColoFontSuave} style={styles.backBtn} onPress={() => navigation.navigate('StartConfigInitial')}/>
+        <Entypo name="chevron-left" size={40} color={ColorGlobal.ColoBtnGradient} style={styles.backBtn} onPress={() => navigation.navigate('StartConfigInitial')}/>
           
           
           <View style={styles.boxImg}>
