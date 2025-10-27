0cd4db50ca8e5288a1e16c81a0259238ba0feec5
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-26 23:52:03 -0300

    update

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     maxWidth: 960,
-    marginHorizontal: "auto",
+    // marginHorizontal: "auto",
   },
   title: {
     fontSize: 64,
