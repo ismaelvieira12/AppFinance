e0428939c6c4272af54843ddb7a88e8f874589fe
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-26 23:51:33 -0300

    Update

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ export default function Page() {
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>.</Text>
+        <Text style={styles.subtitle}>Teste</Text>
       </View>
     </View>
   );
