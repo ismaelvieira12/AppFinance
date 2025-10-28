61ca6fe57ae5056f4d488aa3061e31a7d730e283
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-28 20:01:12 -0300

    Deu certo

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import 
+import {Routes} from '../routes'
 const index = () => {
   
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize:40}}>index</Text>
-    </View>
+    <>
+      <Routes />
+    </>
   )
 }
 
