32bd407aa69563fafd28f2506e63b0063d464d18
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-27 23:32:46 -0300

    Mudando o começo das minhas rotas

--- app/index.js
+++ app/index.js
@@ -3,9 +3,7 @@ import React from 'react'
 
 const index = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={styles.text}>Testando</Text>
-    </View>
+   
   )
 }
 
