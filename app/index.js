0b4b171196a8ae27908e2a8ee6f35eb7df3f3f1e
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-27 23:44:59 -0300

    Update

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import App from './App'
-import navigaton from './App'
+import { useNavigation } from '@react-navigation/native';
 const index = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
