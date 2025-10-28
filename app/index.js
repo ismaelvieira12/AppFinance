a3fafff4aea5f569d2a43e2764845182917a201c
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-27 23:48:33 -0300

    Mudando o modo de navegação para o navigation mesmo

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-// import { useNavigation } from '@react-navigation/native';
+
 const index = () => {
-  // const navigation = useNavigation();
+  
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text style={{fontSize:40}}>index</Text>
