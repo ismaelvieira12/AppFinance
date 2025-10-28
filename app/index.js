c501c2b7ea8558e4923d086cf6900b476fc67022
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-28 20:17:24 -0300

    Update

--- app/index.js
+++ app/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-// import {Routes} from '../routes'
-import App from '../App'
+import { Link } from 'expo-router'
 const index = () => {
   
   return (
-    <>
-      <App />
-      {/* <Routes /> */}
-    </>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 40}}>Index</Text>
+      <Link href="">Ir para StartConfigInitial</Link>
+    </View>
   )
 }
 
