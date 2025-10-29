9bd53158706a4a7ea093c9f6899e06fdd7b67d39
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-28 22:02:11 -0300

    Tudo certo

--- app/index.js
+++ app/index.js
@@ -1,9 +1,60 @@
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { useNavigation } from '@react-navigation/native';
+import { StatusBar, Image, Text } from 'react-native'
+import { styles } from '../src/Screens/StartConfigInitial/StartConfigInitialStyle'
+import { LinearGradient } from 'expo-linear-gradient';
+import { ColorGlobal } from '../src/paletaColor/ColorGlobal';
 
-export default function Index() {
+export default function StartConfigInitial() {
+  // const navigation = useNavigation();
+
+  // const handleFinalizarConfig = async () => {
+  //   try {
+  //     // Salva flag de que o usuário já configurou
+  //     await AsyncStorage.setItem('configInicial', 'true');
+
+  //     // Redireciona para Home
+  //     navigation.navigate('Home');
+  //   } catch (error) {
+  //     console.error('Erro ao salvar configuração:', error);
+  //   }
+  // };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Welcome to the App!</Text>
+   <View style={styles.container}>
+      <View style={styles.boxImage}>
+        <Image
+          source={require('../assets/imgs/imagem-inicialPNG03.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.boxInfor}>
+        <View>
+          <Text style={styles.title}>Um futuro mais seguro para você</Text>
+        </View>
+        <Text style={styles.subTitle}>Tendo previsibilidade financeira você garante uma vida muito mais tranquila e segura.</Text>
+      </View>
+
+      <View style={styles.boxLogin}>
+        <LinearGradient
+          colors={[ColorGlobal.AzulNormal, ColorGlobal.ColoBtnGradient]}
+          start={{ x: 1, y: 0.5 }}
+          end={{ x: 0, y: 0.5 }}
+          style={styles.btnGradient}
+        >
+          <TouchableOpacity><Text style={styles.textBtn}>Criar uma conta</Text></TouchableOpacity>
+        </LinearGradient>
+
+        <View style={styles.boxTextsLogin}>
+          <Text style={styles.textLogin}>Já tem uma conta? </Text>
+          <TouchableOpacity><Text style={styles.textFazerLogin}>Fazer login</Text></TouchableOpacity>
+        </View>
+      </View>
+      <StatusBar barStyle="dark-content" />
     </View>
   );
-}
+};
