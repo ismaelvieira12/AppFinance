7868c1e9bf0749f2738f4a926942ddb5511eda9e
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-28 22:13:49 -0300

    Update

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import { Link } from "expo-router";
 export default function Login ()  {
     return (
         <View>
+            <Link href="/index">Go to Register</Link>
             <Text>Login Screen</Text>
         </View>
     );
