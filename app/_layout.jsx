98e32180b4a6c453f931984662092ebe7739ad66
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-29 21:38:06 -0300

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,18 @@
 import { Link } from "expo-router";
 import React from "react";
+import { Stack } from "expo-router/stack";
+
 
 export default function Layout() {
   return (
     <>
-        <Link href="/" />
+        <Stack>
+            <Stack.Screen 
+                options={{ headerShown: false }} 
+            />
+        </Stack>
+        <Link href="/index" />
+        
     </>
   );
 }
