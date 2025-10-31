0293d8d6bb83828eaa93d60499185b32974de399
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-30 23:09:52 -0300

    feat: add Register screen and update layout to include it

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,6 +5,7 @@ export default function Layout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="Wellcome" options={{ headerShown: false }} />
         <Stack.Screen name="auth/Login" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/Register" options={{ headerShown: false }} />
     </Stack>
   );
 }
