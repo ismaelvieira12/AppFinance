2836f7bd0685be578c9543f6635fe8a3396a8604
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-30 23:14:20 -0300

    fix: uncomment necessary imports in Login component

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,19 +1,19 @@
-// import React from 'react';
-  // import {
-  //   View,
-  //   Text,
-  //   Image,
-  //   TextInput,
-  //   TouchableOpacity,
-  //   KeyboardAvoidingView,
-  //   TouchableWithoutFeedback,
-  //   Keyboard,
-  //   Platform,
-  //   StatusBar,
-  //   ImageBackground,
-  // } from 'react-native';
-  // import { LinearGradient } from 'expo-linear-gradient';
-  // import { styles } from './ScreenLoginStyle';
+import React from 'react';
+  import {
+    View,
+    Text,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Platform,
+    StatusBar,
+    ImageBackground,
+  } from 'react-native';
+  import { LinearGradient } from 'expo-linear-gradient';
+  import { styles } from '../../src/Screens/login/ScreenLoginStyle';
   // import { ColorGlobal } from '../../paletaColor/ColorGlobal'; 
   // import { useNavigation } from '@react-navigation/native';
   // import { BackBtn } from '../../components/botaoBack/BackBtn';
