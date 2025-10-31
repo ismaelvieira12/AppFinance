2a2969b698961568a498af316c4cc3a00e14b64c
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-10-30 23:13:25 -0300

    fix: comment out unused imports in Login component

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,5 +1,22 @@
-import React from "react";
-import { View, Text } from "react-native";
+// import React from 'react';
+  // import {
+  //   View,
+  //   Text,
+  //   Image,
+  //   TextInput,
+  //   TouchableOpacity,
+  //   KeyboardAvoidingView,
+  //   TouchableWithoutFeedback,
+  //   Keyboard,
+  //   Platform,
+  //   StatusBar,
+  //   ImageBackground,
+  // } from 'react-native';
+  // import { LinearGradient } from 'expo-linear-gradient';
+  // import { styles } from './ScreenLoginStyle';
+  // import { ColorGlobal } from '../../paletaColor/ColorGlobal'; 
+  // import { useNavigation } from '@react-navigation/native';
+  // import { BackBtn } from '../../components/botaoBack/BackBtn';
 
 export default function Login ()  {
     return (
