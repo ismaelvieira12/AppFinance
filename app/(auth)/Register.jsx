8e6133b072f84abb561551a0ce60830e50298692
Ismael Nascimento <ismaelvieira12@outlook.com>
2025-11-01 11:22:31 -0300

    Criando novamente a pagina de register

--- app/(auth)/Register.jsx
+++ app/(auth)/Register.jsx
@@ -1,10 +1,106 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// import React from 'react';
+// import {
+//   View,
+//   Text,
+//   Image,
+//   TextInput,
+//   TouchableOpacity,
+//   KeyboardAvoidingView,
+//   TouchableWithoutFeedback,
+//   Keyboard,
+//   Platform,
+//   StatusBar,
+// } from 'react-native';
+// import { LinearGradient } from 'expo-linear-gradient';
+// import { styles } from './ScreenRegisterStyle';
+// import { ColorGlobal } from '../../paletaColor/ColorGlobal';
+// import { useNavigation } from '@react-navigation/native';
+// import { BackBtn } from '../../components/botaoBack/BackBtn';
 
-export default function Register() {
-  return (
-    <View>
-      <Text>Register</Text>
-    </View>
-  )
-}
+// export const ScreenRegister = () => {
+//   const navigation = useNavigation();
+
+//   return (
+//     <KeyboardAvoidingView
+//       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+//       style={styles.container}
+//     >
+//         <BackBtn />
+//       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+//         <View
+//           style={{
+//             flex: 1,
+//             width: '100%',
+//             justifyContent: 'center',
+//             alignItems: 'center',
+//             padding: 40,
+//           }}
+//         >
+//           {/* Imagem */}
+//           <View style={styles.boxImg}>
+//             <Image
+//               source={require('../../../assets/imgs/perfilRegister.png')}
+//               style={styles.img}
+//             />
+//           </View>
+
+//           {/* Campos de cadastro */}
+//           <View>
+//             <TextInput placeholder="Nome completo" style={styles.nputLogin} />
+//             <TextInput
+//               placeholder="Email"
+//               keyboardType="email-address"
+//               style={styles.nputLogin}
+//             />
+//             <TextInput
+//               placeholder="Senha"
+//               secureTextEntry
+//               style={styles.nputLogin}
+//             />
+//             <TextInput
+//               placeholder="Confirmar senha"
+//               secureTextEntry
+//               style={styles.nputLogin}
+//             />
+
+//             {/* Botão de criar conta */}
+//             <TouchableOpacity>
+//               <LinearGradient
+//                 colors={[ColorGlobal.AzulNormal, ColorGlobal.ColoBtnGradient]}
+//                 start={{ x: 1, y: 0.5 }}
+//                 end={{ x: 0, y: 0.5 }}
+//                 style={[styles.nputLogin, styles.btnBottomLogin]}
+//               >
+//                 <Text style={styles.textBtnLogin}>Cadastrar</Text>
+//               </LinearGradient>
+//             </TouchableOpacity>
+
+//             {/* Texto de navegação */}
+//             <View
+//               style={{
+//                 flexDirection: 'row',
+//                 justifyContent: 'center',
+//                 marginTop: 10,
+//               }}
+//             >
+//               <Text style={{ fontSize: 16 }}>Já possui uma conta? </Text>
+//               <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+//                 <Text
+//                   style={{
+//                     color: ColorGlobal.AzulMaisClaro,
+//                     fontWeight: 'bold',
+//                     fontSize: 16,
+//                   }}
+//                 >
+//                   Faça login
+//                 </Text>
+//               </TouchableOpacity>
+//             </View>
+//           </View>
+
+//           <StatusBar barStyle="dark-content" />
+//         </View>
+//       </TouchableWithoutFeedback>
+//     </KeyboardAvoidingView>
+//   );
+// };
