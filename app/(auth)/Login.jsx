cc4f501de0c0aea6a2effde32660569ad9ef1fa8
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-01 23:35:17 -0300

    Update

--- app/(auth)/Login.jsx
+++ app/(auth)/Login.jsx
@@ -1,88 +1,77 @@
-// import React from 'react';
-//   import {
-//     View,
-//     Text,
-//     Image,
-//     TextInput,
-//     TouchableOpacity,
-//     KeyboardAvoidingView,
-//     TouchableWithoutFeedback,
-//     Keyboard,
-//     Platform,
-//     StatusBar,
-//     ImageBackground,
-//   } from 'react-native';
-//   import { LinearGradient } from 'expo-linear-gradient';
-//   import { styles } from '../../src/Screens/login/ScreenLoginStyle';
-//   import { ColorGlobal } from '../../src/paletaColor/ColorGlobal'; 
-// //   import { useNavigation } from '@react-navigation/native';
-//   import { BackBtn } from '../../src/components/botaoBack/BackBtn.jsx';
+import React from 'react';
+  import {
+    View,
+    Text,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Platform,
+    StatusBar,
+    ImageBackground,
+  } from 'react-native';
+  import { LinearGradient } from 'expo-linear-gradient';
+  import { styles } from '../../src/Screens/login/ScreenLoginStyle';
+  import { ColorGlobal } from '../../src/paletaColor/ColorGlobal'; 
+//   import { useNavigation } from '@react-navigation/native';
+  import { BackBtn } from '../../src/components/botaoBack/BackBtn.jsx';
 
-// export default function Login ()  {
-//       return (
-//         <KeyboardAvoidingView
-//         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-//         style={styles.container}
-//         >
-//           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-//             <View style={{flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center', padding: 40}}>
-//               {/* Botão de voltar */}
-//               <BackBtn />
+export default function Login ()  {
+      return (
+        <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+        >
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={{flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center', padding: 40}}>
+              {/* Botão de voltar */}
+              <BackBtn />
 
-//               <View style={styles.boxImg}>
-//                 <ImageBackground
-//                   source={require('../../assets/imgs/segunda_foto_perfil.png')}
-//                   style={styles.img}
-//                 />
-//               </View>
+              <View style={styles.boxImg}>
+                <ImageBackground
+                  source={require('../../assets/imgs/segunda_foto_perfil.png')}
+                  style={styles.img}
+                />
+              </View>
 
-//               {/* Campos de login */}
-//               <View>
-//                   <TextInput placeholder="Email" style={styles.nputLogin} />
-//                   <TextInput
-//                     placeholder="Senha"
-//                     secureTextEntry
-//                     style={styles.nputLogin}
-//                   />
+              {/* Campos de login */}
+              <View>
+                  <TextInput placeholder="Email" style={styles.nputLogin} />
+                  <TextInput
+                    placeholder="Senha"
+                    secureTextEntry
+                    style={styles.nputLogin}
+                  />
 
-//                   {/* Botão */}
-//                   <TouchableOpacity>
-//                     <LinearGradient
-//                       colors={[ColorGlobal.AzulNormal, ColorGlobal.ColoBtnGradient]}
-//                       start={{ x: 1, y: 0.5 }}
-//                       end={{ x: 0, y: 0.5 }}
-//                       style={[styles.nputLogin, styles.btnBottomLogin]}
-//                     >
-//                       <Text style={styles.textBtnLogin}>Fazer login</Text>
-//                     </LinearGradient>
-//                   </TouchableOpacity>
+                  {/* Botão */}
+                  <TouchableOpacity>
+                    <LinearGradient
+                      colors={[ColorGlobal.AzulNormal, ColorGlobal.ColoBtnGradient]}
+                      start={{ x: 1, y: 0.5 }}
+                      end={{ x: 0, y: 0.5 }}
+                      style={[styles.nputLogin, styles.btnBottomLogin]}
+                    >
+                      <Text style={styles.textBtnLogin}>Fazer login</Text>
+                    </LinearGradient>
+                  </TouchableOpacity>
 
-//                   {/* Texto abaixo */}
-//                   <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
-//                     <Text style={{fontSize: 16}}>Ainda não possui uma conta? </Text>
-//                     <TouchableOpacity >
-//                       <Text style={{ color: ColorGlobal.AzulMaisClaro, fontWeight: 'bold',fontSize: 16 }}>
-//                         Cadastre-se
-//                       </Text>
-//                     </TouchableOpacity>
-//                   </View>
-//               </View>
+                  {/* Texto abaixo */}
+                  <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
+                    <Text style={{fontSize: 16}}>Ainda não possui uma conta? </Text>
+                    <TouchableOpacity >
+                      <Text style={{ color: ColorGlobal.AzulMaisClaro, fontWeight: 'bold',fontSize: 16 }}>
+                        Cadastre-se
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+              </View>
 
-//               <StatusBar barStyle="dark-content" />
+              <StatusBar barStyle="dark-content" />
 
-//             </View>
-//           </TouchableWithoutFeedback>
-//         </KeyboardAvoidingView>
-//       );
-// }
-
-import { View, Text } from 'react-native'
-import React from 'react'
-
-export default function Login() {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      );
 }
