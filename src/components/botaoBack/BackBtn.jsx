eda60669bda143ccb49b9e88c491fda85ac70acc
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-02 23:50:55 -0300

    Update

--- src/components/botaoBack/BackBtn.jsx
+++ src/components/botaoBack/BackBtn.jsx
@@ -1,18 +1,17 @@
 
 import React from 'react'
-import { TouchableOpacity, ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { ColorGlobal } from '../../paletaColor/ColorGlobal';
-import { View } from 'react-native-web';
 
 
 export const BackBtn = () => {
     const navigation = useNavigation();
   return (
-    <ScrollView>
-      <View style={styles.box1}
-    </ScrollView>
+    <TouchableOpacity style={styles.backBtn} onPress={() => navigation.goBack()}>
+        <Entypo name="chevron-left" size={32} color={ColorGlobal.AzulMaisClaro} />
+    </TouchableOpacity>
   )
 }
 
@@ -23,7 +22,4 @@ const styles = {
         left: 20,
         zIndex: 10,
     },
-    box1: {
-      
-    }
 };
