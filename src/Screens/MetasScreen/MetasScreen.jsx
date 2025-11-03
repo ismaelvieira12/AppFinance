6aeb087ab423346ac323be10908c8d95d7eb974d
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-02 23:54:56 -0300

    Update

--- src/Screens/MetasScreen/MetasScreen.jsx
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, TextInput, StyleSheet, ScrollView } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+// import { LinearGradient } from "expo-linear-gradient";
 import { BackBtn } from '../../components/botaoBack/BackBtn';
 
 
@@ -8,8 +8,8 @@ export default function LoginScreen() {
   return (
     <ScrollView style={styles.container}>
         <BackBtn />
-        <View>
-            
+        <View style={styles.box1}>
+            <Text>METAS</Text>
         </View>
     </ScrollView>
   );
@@ -17,12 +17,9 @@ export default function LoginScreen() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center" },
-  form: { marginBottom: 150 },
-  input: {
-    backgroundColor: "rgba(255,255,255,0.15)",
-    borderRadius: 25,
-    padding: 15,
-    marginVertical: 10,
-    color: "#fff",
-  },
+ box1: {
+    backgroundColor: "red",
+    height: 200,
+    
+ }
 });
