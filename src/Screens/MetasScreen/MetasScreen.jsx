cba65750bd1ae27f1addaf8933bc83a3220988d6
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-03 23:55:28 -0300

    Update

--- src/Screens/MetasScreen/MetasScreen.jsx
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function MetasScreen() {
   return (
     <ScrollView>
-      
+      <View style={styles.container}></View>
       <Text>MetasScreen</Text>
     </ScrollView>
   )
@@ -14,5 +14,9 @@ export default function MetasScreen() {
 const styles = {
   container: {
     backgrounfColor: '#FA4000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 300',
+    weidth: 300,
   },
 }
