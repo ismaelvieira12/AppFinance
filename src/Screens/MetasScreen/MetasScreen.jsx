374902ee2050770ac3cb194dfaea2f796c1c2c80
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-03 23:46:07 -0300

    Deu certo

---
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MetasScreen() {
+  return (
+    <View>
+      <Text>MetasScreen</Text>
+    </View>
+  )
+}
