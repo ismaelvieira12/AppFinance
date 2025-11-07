b30a15251507ee458be5e0b4eeadb30c008affe4
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-06 23:48:35 -0300

    Update

---
+++ src/components/drawer/DrawerRouts.jsx
@@ -0,0 +1,16 @@
+// DrawerRoutes.js
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Wellcome } from '../Screens/Wellcome/Wellcome';
+import MetasScreen from '../Screens/MetasScreen/MetasScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerRoutes() {
+  return (
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen name="Home" component={Wellcome} />
+      <Drawer.Screen name="Metas" component={MetasScreen} />
+    </Drawer.Navigator>
+  );
+}
