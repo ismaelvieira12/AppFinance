b238571b03e239a2d8daa28d6d6aa3f86613ac34
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-07 23:50:33 -0300

    Update

--- src/Screens/planejamentoMensal/StylePlanejamento.js
+++ src/Screens/planejamentoMensal/StylePlanejamento.js
@@ -1,7 +1,12 @@
+import { StyleSheet } from "react-native";
+import { ColorGlobal } from "../../paletaColor/ColorGlobal";
+
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: ColorGlobal.TesteColor,
         padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
     }, 
 });
