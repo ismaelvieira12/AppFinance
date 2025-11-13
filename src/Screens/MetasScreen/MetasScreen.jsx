71117e1369fa424643aa53ccc11ce9936f3a77c4
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-12 22:58:49 -0300

    Update

--- src/Screens/MetasScreen/MetasScreen.jsx
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -128,77 +128,79 @@ export default function MetasScreen() {
         
       </View>
 
-      {/* Inputs */}
-      <TextInput
-        placeholder="Descrição"
-        value={descricao}
-        onChangeText={setDescricao}
-        style={styles.input}
-      />
+      <View style={{ paddingHorizontal: 15 }}>
+        {/* Inputs */}
+        <TextInput
+          placeholder="Descrição"
+          value={descricao}
+          onChangeText={setDescricao}
+          style={styles.input}
+        />
 
-      <TextInput
-        placeholder="Valor"
-        value={valor}
-        onChangeText={handleValorChange}
-        keyboardType="numeric"
-        style={styles.input}
-      />
+        <TextInput
+          placeholder="Valor"
+          value={valor}
+          onChangeText={handleValorChange}
+          keyboardType="numeric"
+          style={styles.input}
+        />
 
-      <View style={styles.tipoContainer}>
-        <TouchableOpacity
-          style={[styles.btnTipo, tipo === "receita" && styles.btnSelecionado]}
-          onPress={() => setTipo("receita")}
-        >
-          <Text style={styles.textBtn}>Receita</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={[styles.btnTipo, tipo === "despesa" && styles.btnSelecionado]}
-          onPress={() => setTipo("despesa")}
-        >
-          <Text style={styles.textBtn}>Despesa</Text>
-        </TouchableOpacity>
-      </View>
-
-      <TouchableOpacity style={styles.btnAdd} onPress={adicionarTransacao}>
-        <Text style={styles.btnAddText}>Adicionar</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={[styles.btnAdd, { backgroundColor: "#E53935", marginTop: 10 }]}
-        onPress={limparTudo}
-      >
-        <Text style={styles.btnAddText}>🗑️ Limpar Tudo</Text>
-      </TouchableOpacity>
-
-      {/* Lista */}
-      <FlatList
-        data={transacoes}
-        keyExtractor={(item) => item.id}
-        style={{ marginTop: 20 }}
-        renderItem={({ item }) => (
-          <View
-            style={[
-              styles.item,
-              {
-                borderLeftColor:
-                  item.tipo === "receita" ? "#4CAF50" : "#E53935",
-              },
-            ]}
+        <View style={styles.tipoContainer}>
+          <TouchableOpacity
+            style={[styles.btnTipo, tipo === "receita" && styles.btnSelecionado]}
+            onPress={() => setTipo("receita")}
           >
-            <Text style={styles.desc}>{item.descricao}</Text>
-            <Text
+            <Text style={styles.textBtn}>Receita</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.btnTipo, tipo === "despesa" && styles.btnSelecionado]}
+            onPress={() => setTipo("despesa")}
+          >
+            <Text style={styles.textBtn}>Despesa</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.btnAdd} onPress={adicionarTransacao}>
+          <Text style={styles.btnAddText}>Adicionar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.btnAdd, { backgroundColor: "#E53935", marginTop: 10 }]}
+          onPress={limparTudo}
+        >
+          <Text style={styles.btnAddText}>🗑️ Limpar Tudo</Text>
+        </TouchableOpacity>
+
+        {/* Lista */}
+        <FlatList
+          data={transacoes}
+          keyExtractor={(item) => item.id}
+          style={{ marginTop: 20 }}
+          renderItem={({ item }) => (
+            <View
               style={[
-                styles.valor,
-                { color: item.tipo === "receita" ? "#4CAF50" : "#E53935" },
+                styles.item,
+                {
+                  borderLeftColor:
+                    item.tipo === "receita" ? "#4CAF50" : "#E53935",
+                },
               ]}
             >
-              {item.tipo === "despesa" ? "-" : "+"} R${" "}
-              {item.valor.toFixed(2).replace(".", ",")}
-            </Text>
-          </View>
-        )}
-      />
+              <Text style={styles.desc}>{item.descricao}</Text>
+              <Text
+                style={[
+                  styles.valor,
+                  { color: item.tipo === "receita" ? "#4CAF50" : "#E53935" },
+                ]}
+              >
+                {item.tipo === "despesa" ? "-" : "+"} R${" "}
+                {item.valor.toFixed(2).replace(".", ",")}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
