8e6a7855cf7b19b5a2b95339d11ae8222cc441f7
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-18 23:40:33 -0300

    Update

--- src/Screens/MetasScreen/MetasScreen.jsx
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -156,7 +156,7 @@ export default function MetasScreen() {
       </View>
 
     </View>
-      <TouchableOpacity style={{backgroundColor: ColorGlobal.AmareloNormal}}>
+      <TouchableOpacity style={{backgroundColor: ColorGlobal.AmareloNormal, height: 40}}>
         
       </TouchableOpacity>
 
