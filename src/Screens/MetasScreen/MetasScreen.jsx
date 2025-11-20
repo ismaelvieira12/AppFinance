1653f11f2505c0b84578e1adb88b116088d72541
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-19 23:30:20 -0300

    Update

--- src/Screens/MetasScreen/MetasScreen.jsx
+++ src/Screens/MetasScreen/MetasScreen.jsx
@@ -234,7 +234,7 @@ export default function MetasScreen() {
         />
       </View>
       <TouchableOpacity  style={{backgroundColor: ColorGlobal.AzulEscuro, height: 40, width: 80, borderRadius: 15, position: "absolute", bottom: 10, left: 20, justifyContent: "center", alignItems:"center"}} onPress={() => navigation.goBack()}>
-              <Text style={{color: ColorGlobal.FundoCards, fontWeight: "bold"}}>Voltar</Text>
+        <Text style={{color: ColorGlobal.FundoCards, fontWeight: "bold"}}>Voltar</Text>
       </TouchableOpacity>
     </View>
   );
