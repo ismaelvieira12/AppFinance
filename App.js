fb56483a5066f18645590aaacb52419dd47882b9
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2025-11-26 23:56:10 -0300

    Update

--- App.js
+++ App.js
@@ -46,15 +46,7 @@ export default function App() {
     verificarConfiguracao();
   }, []);
 
-  // Exibe loading enquanto verifica
-  if (isConfigured === null) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color={ColorGlobal.AzulNormal} />
-
-      </View>
-    );
-  }
+ 
 
   return (
     <NavigationContainer>
